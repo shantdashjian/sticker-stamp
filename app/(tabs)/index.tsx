d6dcad9f8fb805bab39e6deeaf09541fed2a9b4f
Shant Dashjian <shantgdashjian@gmail.com>
2024-11-29 12:33:33 +0400

    Add the image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,45 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
-import Button from '@/components/Button';
 
-const PlaceholderImage = require('@/assets/images/background-image.jpeg');
-
+const PlaceholderImage = require("@/assets/images/background-image.jpeg");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
@@ -22,14 +49,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#23a',
+    alignItems: "center",
+    backgroundColor: "#23a",
   },
   imageContainer: {
     flex: 1,
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: 'center',
+    alignItems: "center",
   },
-})
+});
