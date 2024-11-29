e695e04aa8b4e576d93af3e8411b153963e1c0f8
Shant Dashjian <shantgdashjian@gmail.com>
2024-11-29 10:34:24 +0400

    Add navigation and about and not found pages

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -16,6 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#65f'
   },
   text: {
-    color: '#fff'
-  }
-})
+    color: '#fff',
+  },
+});
